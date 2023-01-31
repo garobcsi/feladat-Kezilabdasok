e826a535c1ea1a91a90d841e3406d8c9da7dfa6c
garobcsi <gasparrobert0908@gmail.com>
2023-01-31 13:16:46 +0100

    Made Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CsapatController;
+use App\Http\Controllers\JatekosController;
 
 /*
 |--------------------------------------------------------------------------
